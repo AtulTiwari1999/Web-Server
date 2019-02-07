a34241b71584fd5ecc68d16591aecc5b4ce36a9c
A7 <41237264+AtulTiwari1999@users.noreply.github.com>
2019-02-07 15:52:19 +0530

    Update main.rs

--- main.rs
+++ main.rs
@@ -11,7 +11,7 @@ use std::time::Duration;
 
 
 fn main() {
- let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); 
+ let listener = TcpListener::bind("127.0.0.1:").unwrap(); 
  let pool = ThreadPool::new(4);
 
   for stream in listener.incoming().take(2) {
@@ -33,13 +33,13 @@ let get = b"GET / HTTP/1.1\r\n";
 let sleep = b"GET /sleep HTTP/1.1\r\n";
 
  let (status_line, filename)= if buffer.starts_with(get) {
-  ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
+  ("HTTP/1.1 200 OK\r\n\r\n","index.html")
 } else if buffer.starts_with(sleep) {
    thread::sleep(Duration::from_secs(5));
-   ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
+   ("HTTP/1.1 200 OK\r\n\r\n","index.html")
 
 }else { 
-      ("HTTP/1.1 404 NOT FOUND\r\n\r\n","404.html")
+      ("HTTP/1.1 404 NOT FOUND\r\n\r\n","error404.html")
 
 
 };
